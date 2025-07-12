13dbfe63fa079d84c80d848782cb20c6d947b835
werneregk <info@keepiolo.com>
2025-07-12 16:13:04 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Central Nortron'
+project = 'firewallnortnn'
 copyright = '2025'
-author = 'Michael Beheim'
+author = 'Richard Euringer'
 
 release = '1.0'
 
